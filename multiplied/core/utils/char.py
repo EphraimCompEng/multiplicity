54405546b1722adde61ecf6c6c6cb97fe1dc9fab
EphraimCompEng <madugba.finance@proton.me>
2026-01-17 02:22:41 +0000

    Implement charttf() generator

--- multiplied/core/utils/char.py
+++ multiplied/core/utils/char.py
@@ -14,19 +14,47 @@ def ischar(ch:str) -> bool:
         return False
 
 def chargen() -> Generator[str]:
+    """
+    Continuously generate characters from A to Z.
+    """
     i = 0
     while True:
         yield chr((i % 26) + 65)
         i += 1
+
+def chartff(char_) -> Generator[str]:
+    """
+    Infinitely generate char in upper then lowercase form.
+
+    >>> x = chartff('a')
+    >>> next(x)
+    'a'
+    >>> next(x)
+    'A'
+    >>> next(x)
+    'a'
+
+    """
+    i = True
+    while True:
+        if i := not(i):
+            yield char_.lower()
+        else:
+            yield char_.upper()
+
 
 
 
 
 # -- testing --------------------------------------------------------
 def main():
-    test = chargen()
+    testgen = chargen()
     for _ in range(32):
-        print(next(test))
+        tmp = next(testgen)
+        testtff = chartff(tmp)
+        for _ in range(8):
+            print(next(testtff), end='')
+        print(tmp)
 
 if __name__ == "__main__":
     main()
