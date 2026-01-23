9481cb7edb830850f671793d053b033ae0ae5599
EphraimCompEng <madugba.finance@proton.me>
2026-01-23 22:24:38 +0000

    Update test_algorithm.py

--- multiplied/tests/test_algorithm.py
+++ multiplied/tests/test_algorithm.py
@@ -17,12 +17,7 @@ def gen_resources(bits: int, *, a=0, b=0
 
 def test_step() -> None:
     m = mp.Matrix(4, a=5, b=4)
-    p = mp.Pattern([
-        'a',
-        'a',
-        'b',
-        'b',
-    ])
+    p = mp.Pattern(['a','a','b','b'])
     alg = mp.Algorithm(m)
     alg.push(p)
     alg.step()
@@ -32,12 +27,7 @@ def test_step() -> None:
 
 def test_auto_resolve_single_4() -> None:
     m = mp.Matrix(4)
-    p = mp.Pattern([
-        'a',
-        'a',
-        'b',
-        'b',
-    ])
+    p = mp.Pattern(['a','a','b','b'])
     alg = mp.Algorithm(m)
     alg.push(p)
     print(alg)
@@ -54,9 +44,9 @@ def test_manual_population_8() -> None:
     alg.push(p)
     print(alg)
     # alg.auto_resolve_pattern(p, m)
-    t2 = mp.Template(mp.Pattern(['a','a','b','b','c','d','e','f']), matrix=alg.algorithm[0]['pseudo'])
+    t2 = mp.Template(mp.Pattern(['a','a','b','b','_','_','_','_']), matrix=alg.algorithm[0]['pseudo'])
     alg.push(t2)
-    t3 = mp.Template(mp.Pattern(['a','a','b','c','d','e','f', 'g']), matrix=alg.algorithm[1]['pseudo'])
+    t3 = mp.Template(mp.Pattern(['a','a','_','_','_','_','_','_']), matrix=alg.algorithm[1]['pseudo'])
     alg.push(t3)
     # print(alg)
 
@@ -76,9 +66,9 @@ def test_isolate_arithmetic_units() -> None:
     print(isolated_units)
 
 def main():
-    # test_manual_population_8()
-    # test_auto_resolve_recursive_full()
-    # test_isolate_arithmetic_units()
+    test_manual_population_8()
+    test_auto_resolve_recursive_full()
+    test_isolate_arithmetic_units()
     m = mp.Matrix(8)
     p = mp.Pattern(['a','a','b','b','c','c','d','d'])
     alg = mp.Algorithm(m)
