c3dbf60d2846741ae045c74b8926908ca6d3f47f
EphraimCompEng <madugba.finance@proton.me>
2026-02-08 18:42:28 +0000

    Implemented pq_extract_stages()

--- multiplied/analysis/extract.py
+++ multiplied/analysis/extract.py
@@ -11,6 +11,8 @@ import pyarrow as pa
 # cols = [("stage_1","ppm_0",f"b{i}") for i in range(8)]
 # df   = pd.read_parquet("xyz.parquet", columns=cols)
 #
+# idx = pd.IndexSlice
+#
 # df.loc[:, idx["stage_1", :, :]]        # all ppms & bits for stage_1
 # df.loc[:, idx[:, "ppm_0", :]]          # that ppm across all stages
 # df.loc[:, idx[:, :, "b3"]]             # bit b3 across all stages/ppms
@@ -47,21 +49,42 @@ def pq_extract_stages(path: str, *, stages: list[str]=[]) -> pd.DataFrame:
     pf = ParquetFile(path)
     first = next(pf.iter_batches(batch_size = 1))
     row = pa.Table.from_batches([first]).to_pandas()
-    columns = row.columns
-    print(columns)
+    pd.set_option('display.max_columns', None)
+
+    print(row)
+
+    # Multiplied datasets will always include formatted string columns
+    # with the rightmost columns dedicated to formatted strings.
+    # Hence the rightmost column is the final formatted string column
+    final_ppm_s_column = row.columns[-1]
+
+    # This is not optimal at all -- problem for future me ----------- #
+    # TODO: manage metadata for .parquet <> DataFrame
+    # trim and extract integer
+    total_stages = int(final_ppm_s_column.split('_')[1][1:])
+
+    # lists[str] converted to str by pandas, find length of first entry
+    bits = row.loc[0]['ppm_s0'][2:].index("'") >> 1
+    print(total_stages,  bits)
+    # --------------------------------------------------------------- #
+
     # loop through stages and push to DataFrame
 
+    if stages == []:
+        stages = [f"stage_{s}" for s in range(total_stages)]
 
-    # bits = row.attrs['bits']
-    # stages = df.attrs['stages']
+    columns = []
+    for s in stages:
+        for p in range(bits):
+            for b in range((bits << 1)-1, -1, -1):
+                # TODO: flatten columns to strings since they're converted anyway
+                # e.g: 'stage_0_ppm_4_b_3'
+                columns.append(str((s, f"ppm_{p}" , f"b{b}")))
 
-    # if stages == []:
-    #     cols = [f"stage_{x}" for x in range(stages)]
-    #     ...
-
-    # else:
-    #     cols = [f"stage_{x}" for x in stages]
-    #     ...
+    # Initialize DataFrame with stages as columns
+    df = pd.read_parquet(path, columns=columns)
+    print(df)
+    return df
 
 
 
--- multiplied/tests/test_analysis.py
+++ multiplied/tests/test_analysis.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import multiplied as mp
 import pandas as pd
 import pyarrow as pa
@@ -5,16 +6,26 @@ import pyarrow as pa
 
 def test_pq_extract_bits() -> None:
     ...
+
+
 def test_pq_extract_stages() -> None:
-    ...
+    path = Path(__file__).parent.parent.parent / 'examples/datasets/example_8b_mult_truthtable.parquet'
+    print(path)
+    df = mp.pq_extract_stages(str(path))
+    # print(df.head())
+
 def test_pq_extract_formatted_all() -> None:
     ...
+
+
 def test_pq_extract_formatted_stages() -> None:
     ...
 
 
+
+
 def main() -> None:
-    ...
+    test_pq_extract_stages()
 
 if __name__ == "__main__":
     main()
