e8e61efc9f1faefe3c12cd4afe40aabd1de43801
EphraimCompEng <madugba.finance@proton.me>
2026-02-08 17:26:26 +0000

    Update test_analysis.py

--- multiplied/tests/test_analysis.py
+++ multiplied/tests/test_analysis.py
@@ -1,3 +1,20 @@
 import multiplied as mp
 import pandas as pd
 import pyarrow as pa
+
+
+def test_pq_extract_bits() -> None:
+    ...
+def test_pq_extract_stages() -> None:
+    ...
+def test_pq_extract_formatted_all() -> None:
+    ...
+def test_pq_extract_formatted_stages() -> None:
+    ...
+
+
+def main() -> None:
+    ...
+
+if __name__ == "__main__":
+    main()
