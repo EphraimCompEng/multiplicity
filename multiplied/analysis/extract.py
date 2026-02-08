5255a3db8bc390d3d346fc6f2b5a9b253d70d743
EphraimCompEng <madugba.finance@proton.me>
2026-02-08 11:40:36 +0000

    Planning analysis modules

--- multiplied/analysis/extract.py
+++ multiplied/analysis/extract.py
@@ -1,3 +1,31 @@
-#######################################
-# Search Truth Table, Then Load To DF #
-#######################################
+##############################################
+# Extract Columns From Multiplied DataFrames #
+##############################################
+
+import multiplied as mp
+import pandas as pd
+import pyarrow as pa
+
+def df_extract_bits(df: pd.DataFrame, bits: list[int], stages: list[int]) -> pd.DataFrame:
+    """
+
+    """
+    ...
+
+def df_extract_columns(df: pd.DataFrame, columns: list[str]) -> pd.DataFrame:
+    """
+
+    """
+    ...
+
+def df_extract_formatted_all(df: pd.DataFrame) -> pd.DataFrame:
+    """
+
+    """
+    ...
+
+def df_extract_formatted_columns(df: pd.DataFrame, stages: list[int]) -> pd.DataFrame:
+    """
+
+    """
+    ...
---
+++ multiplied/tests/test_analysis.py
@@ -0,0 +1,3 @@
+import multiplied as mp
+import pandas as pd
+import pyarrow as pa
